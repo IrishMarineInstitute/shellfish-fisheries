0d0f650e2563bb2525bf11ea098f76b2ae4df795
Guillermo Felipe Martin Gonzalez <53008413+gfmg1992@users.noreply.github.com>
2024-02-22 18:32:11 +0000

    Update web link

--- ui.R
+++ ui.R
@@ -33,19 +33,19 @@ header<- dashboardHeader(title = "Irish Shellfish Fisheries App Visualization",
                                                  from = 'Twitter',
                                                  message = "",
                                                  icon = icon("twitter"),
-                                                 href = "https://shiny.marine.ie/shellfishfisheries/"
+                                                 href = "https://shiny.marine.ie/shellfish-fisheries/"
                                                ),
                                                messageItem(
                                                  from = 'Facebook',
                                                  message = "",
                                                  icon = icon("facebook"),
-                                                 href = "https://shiny.marine.ie/shellfishfisheries/"
+                                                 href = "https://shiny.marine.ie/shellfish-fisheries/"
                                                ),
                                                messageItem(
                                                  from = 'LinkedIn',
                                                  message = "",
                                                  icon = icon("linkedin"),
-                                                 href = "https://shiny.marine.ie/shellfishfisheries/"
+                                                 href = "https://shiny.marine.ie/shellfish-fisheries/"
                                                ))
                          
                          )
